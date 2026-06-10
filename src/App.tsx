b492d1e96091e89b85d5703b763bdb1a849d12e9
lemontreeb-sys <lemontreeb@gmail.com>
2026-06-10 11:03:52 +0900

    fix: App.tsx 안 쓰는 Navigate 임포트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { FormProvider } from "./context/FormContext";
 import MainLayout from "./layouts/MainLayout";
